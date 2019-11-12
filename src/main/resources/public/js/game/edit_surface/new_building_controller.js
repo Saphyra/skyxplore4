e9dad0de3f3059959669c28ffd77c23ad927a0ab
Bernat Bonda <bernat_bonda@epam.com>
2019-11-12 08:47:34 +0100

    Display new building data

--- src/main/resources/public/js/game/edit_surface/new_building_controller.js
+++ src/main/resources/public/js/game/edit_surface/new_building_controller.js
@@ -1,5 +1,7 @@
 (function NewBuildingController(){
+    const buildingDescriptionLocalization = localizations.buildingDescriptionLocalization;
     const buildingLocalization = localizations.buildingLocalization;
+    const resourceLocalization = localizations.resourceLocalization;
 
     window.newBuildingController = new function(){
         this.loadNewBuildingPossibilities = loadNewBuildingPossibilities;
@@ -21,10 +23,72 @@
 
         function createBuildingItem(building){
             const container = document.createElement("div");
+                container.classList.add("new-building-item")
                 const name = document.createElement("h3");
                     name.innerHTML = buildingLocalization.get(building.dataId);
             container.appendChild(name);
+
+                const description = document.createElement("div");
+                    description.classList.add("new-building-description");
+                    description.innerHTML = buildingDescriptionLocalization.get(building.dataId);
+            container.appendChild(description);
+
+                const resourceTable = document.createElement("table");
+                    resourceTable.appendChild(createTableHeader());
+                    resourceTable.appendChild(createWorkPoints(building.constructionRequirements.workPoints));
+            container.appendChild(resourceTable);
+
+                new Stream(entryList(building.constructionRequirements.resources))
+                    .sorted(function(a, b){return resourceLocalization.get(a.getKey()).localeCompare(resourceLocalization.get(b.getKey()))})
+                    .map(createResource)
+                    .forEach(function(item){resourceTable.appendChild(item)});
+
+            container.appendChild(createBuildButton(building.dataId));
             return container;
+
+            function createTableHeader(){
+                const row = document.createElement("TR");
+                    const resourceCell = document.createElement("TH");
+                        resourceCell.innerHTML = Localization.getAdditionalContent("resource");
+                row.appendChild(resourceCell);
+                    const valueCell = document.createElement("TH");
+                        valueCell.innerHTML = Localization.getAdditionalContent("amount");
+                row.appendChild(valueCell);
+                return row;
+            }
+
+            function createWorkPoints(workPoints){
+                const row = document.createElement("tr");
+                    const resourceCell = document.createElement("TD");
+                        resourceCell.innerHTML = Localization.getAdditionalContent("work-points");
+                row.appendChild(resourceCell);
+                    const valueCell = document.createElement("TD");
+                        valueCell.innerHTML = workPoints;
+                row.appendChild(valueCell);
+                return row;
+            }
+
+            function createResource(entry){
+                const row = document.createElement("TR");
+                    const resourceCell = document.createElement("TD");
+                        resourceCell.innerHTML = resourceLocalization.get(entry.getKey());
+                row.appendChild(resourceCell);
+                    const valueCell = document.createElement("TD");
+                        valueCell.innerHTML = entry.getValue();
+                row.appendChild(valueCell);
+                return row;
+            }
+        }
+
+        function createBuildButton(){
+            const button = document.createElement("div");
+                button.classList.add("build-new-building-button");
+                button.classList.add("button");
+                button.innerHTML = Localization.getAdditionalContent("build-building-button");
+                button.onclick = function(){
+                    //TODO implement
+                }
+            return button;
         }
     }
 })();
--- src/main/resources/public/js/game/localizations.js
+++ src/main/resources/public/js/game/localizations.js
@@ -2,6 +2,7 @@
     scriptLoader.loadScript("/js/common/localization/custom_localization.js");
 
     window.localizations = new function(){
+        this.buildingDescriptionLocalization = new CustomLocalization("building_description");
         this.buildingLocalization = new CustomLocalization("building");
         this.resourceLocalization = new CustomLocalization("resource");
         this.storageTypeLocalization = new CustomLocalization("storage_type");
