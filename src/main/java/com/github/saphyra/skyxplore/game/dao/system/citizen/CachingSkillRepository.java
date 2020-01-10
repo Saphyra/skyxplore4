9cb5028e0794afab45fd391ecc35883a00ff16ad
saphyra <bondabear@gmail.com>
2020-01-10 21:10:43 +0100

    CachingSkillRepository

---
+++ src/main/java/com/github/saphyra/skyxplore/game/dao/system/citizen/CachingSkillRepository.java
@@ -0,0 +1,71 @@
+package com.github.saphyra.skyxplore.game.dao.system.citizen;
+
+import com.github.saphyra.skyxplore.common.UuidConverter;
+import com.github.saphyra.skyxplore.common.context.RequestContextHolder;
+import com.github.saphyra.skyxplore.game.dao.common.cache.CacheRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
+@Primary
+@Slf4j
+public class CachingSkillRepository extends CacheRepository<String, SkillEntity, String, SkillRepository> implements SkillRepository {
+    private final RequestContextHolder requestContextHolder;
+    private final UuidConverter uuidConverter;
+
+    protected CachingSkillRepository(SkillRepository repository, RequestContextHolder requestContextHolder, UuidConverter uuidConverter) {
+        super(repository, SkillEntity::getGameId);
+        this.requestContextHolder = requestContextHolder;
+        this.uuidConverter = uuidConverter;
+    }
+
+    @Override
+    protected List<SkillEntity> getByKey(String gameId) {
+        List<SkillEntity> entities = repository.getByGameId(gameId);
+        log.info("SkillEntities loaded by gameId {}: {}", gameId, entities.size());
+        return entities;
+    }
+
+    @Override
+    protected void deleteByIds(List<String> skillIds) {
+        repository.deleteBySkillIdIn(skillIds);
+    }
+
+    @Override
+    public void deleteByGameId(String gameId) {
+        processDeletions();
+        cacheMap.remove(gameId);
+        repository.deleteByGameId(gameId);
+    }
+
+    @Override
+    public void deleteBySkillIdIn(List<String> skillIds) {
+        skillIds.forEach(this::deleteById);
+    }
+
+    @Override
+    public List<SkillEntity> getByCitizenId(String citizenId) {
+        return getMap(getGameId())
+            .values()
+            .stream()
+            .filter(skillEntity -> skillEntity.getCitizenId().equals(citizenId))
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<SkillEntity> getByGameId(String gameId) {
+        //noinspection SimplifyStreamApiCallChains
+        return Optional.ofNullable(cacheMap.get(gameId))
+            .map(map -> map.values().stream().collect(Collectors.toList()))
+            .orElseGet(() -> addToCache(gameId, getByKey(gameId)));
+    }
+
+    private String getGameId() {
+        return uuidConverter.convertDomain(requestContextHolder.get().getGameId());
+    }
+}
