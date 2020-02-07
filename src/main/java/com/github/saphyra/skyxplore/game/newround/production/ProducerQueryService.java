947a6f7c6662a51dee9ed37e78b31acca28c0bea
Bernat_Bonda <bernat_bonda@epam.com>
2020-02-07 09:03:29 +0100

    Check surfaceType when collecting producers

--- src/main/java/com/github/saphyra/skyxplore/game/newround/production/ProducerQueryService.java
+++ src/main/java/com/github/saphyra/skyxplore/game/newround/production/ProducerQueryService.java
@@ -1,13 +1,19 @@
 package com.github.saphyra.skyxplore.game.newround.production;
 
+import com.github.saphyra.skyxplore.data.gamedata.domain.building.production.Production;
+import com.github.saphyra.skyxplore.data.gamedata.domain.building.production.ProductionBuilding;
 import com.github.saphyra.skyxplore.data.gamedata.domain.building.production.ProductionBuildingService;
+import com.github.saphyra.skyxplore.game.dao.map.surface.SurfaceQueryService;
+import com.github.saphyra.skyxplore.game.dao.system.building.Building;
 import com.github.saphyra.skyxplore.game.dao.system.building.BuildingQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.Objects.isNull;
@@ -19,14 +25,27 @@ public class ProducerQueryService {
     private final BuildingQueryService buildingQueryService;
     private final ProducerFactory producerFactory;
     private final ProductionBuildingService productionBuildingService;
+    private final SurfaceQueryService surfaceQueryService;
 
     public List<Producer> getByStarIdAndDataId(UUID starId, String dataId) {
-        return productionBuildingService.values()
+        Map<String, ProductionBuilding> buildings = productionBuildingService.values()
             .stream()
+            //Filter productionBuildings can produce the given resource
             .filter(productionBuilding -> productionBuilding.getGives().containsKey(dataId))
+            .collect(Collectors.toMap(ProductionBuilding::getId, Function.identity()));
+        return buildings.values()
+            .stream()
+            //Query buildings for the producer buildingDataIds
             .flatMap(productionBuilding -> buildingQueryService.getByStarIdAndDataIdAndPlayerId(starId, productionBuilding.getId()).stream())
+            //Filter buildings under construction / update, since they cannot produce
             .filter(building -> isNull(building.getConstructionId()))
+            //Filter buildings placed on proper surfaceType for the production
+            .filter(building -> isOnCorrectSurfaceType(building, buildings.get(building.getBuildingDataId()).getGives().get(dataId)))
             .map(producerFactory::convertToProducer)
             .collect(Collectors.toList());
+    }
+
+    private boolean isOnCorrectSurfaceType(Building building, Production productionBuilding) {
+        return productionBuilding.getPlaced().contains(surfaceQueryService.findBySurfaceIdAndPlayerId(building.getSurfaceId()).getSurfaceType());
     }
 }
